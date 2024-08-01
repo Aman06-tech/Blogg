c6f3707a6f1a640ad6519640fecefdcd556985e0
Aman06-tech <210160203026.aman@gdgu.org>
2024-08-01 17:06:35 +0530

    adding update route api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,13 +17,18 @@ mongoose
   });
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000 ");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
 
 app.use((err, req,res,next)=>{
   const statusCode = err.statusCode || 500;
