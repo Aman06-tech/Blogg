f48aa5982e2d24873a11010a2f60eadcb45893bc
Aman06-tech <210160203026.aman@gdgu.org>
2024-08-13 01:17:24 +0530

    adding post api

---
+++ api/models/post.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+    userId:{
+        type:String,
+        required:true,
+    },
+    content:{
+        type:String,
+        required:true,
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    image: {
+        type:String,
+        default: 'https://cdn.websites.hibu.com/97df2096307b4f18a0f85bf1ed3dceb8/dms3rep/multi/blog-c341ba1a.png',
+    },
+    category: {
+        type: String,
+        default: 'uncategorized',
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+},{timestamps: true}
+);
+
+const  Post = mongoose.model('Post', postSchema);
+
+export default Post;
