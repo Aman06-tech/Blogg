2b2ae458a1b5f94c9a9f4194a589d682bd19dbce
Aman06-tech <210160203026.aman@gdgu.org>
2024-08-15 18:32:53 +0530

    added create post api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import postRoute  from  "./routes/post.route.js";
+import postRoutes from  "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -29,7 +29,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/post", postRoute);
+app.use("/api/post", postRoutes);
 
 
 app.use((err, req,res,next)=>{
