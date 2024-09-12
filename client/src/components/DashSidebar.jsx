b70699ff9c00695f758f96ff542461b631393c8b
Aman06-tech <210160203026.aman@gdgu.org>
2024-09-12 13:15:17 +0530

    add posts section to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,13 +1,15 @@
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function DashSidebar() {
   const location = useLocation();
   const dispatch = useDispatch();
+  const {currentUser} = useSelector((state) => state.user);
   const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -17,41 +19,52 @@ export default function DashSidebar() {
     }
   }, [location.search]);
 
-  const handleSignout = async ()=>{
+  const handleSignout = async () => {
     try {
-      const res = await fetch('/api/user/signout',{
-        method: 'POST',
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
       });
       const data = await res.json();
-      if(!res.ok){
-        console.log(data.message)
-      }else{
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
         dispatch(signoutSuccess());
       }
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-2">
           <Sidebar.Item
             as={Link}
             to="/dashboard?tab=profile"
-            active={tab === 'profile'}
+            active={tab === "profile"}
             icon={HiUser}
-            label={'user'}
+            label={currentUser.isAdmin ? 'Admin' : 'User'}
             labelColor="dark"
           >
             Profile
           </Sidebar.Item>
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer" onClick={handleSignout}>
+          {currentUser.isAdmin && (
+          <Link to="/dashboard?tab=posts">
+            <Sidebar.Item active={tab === "posts"} icon={HiDocumentText} as='div'>
+              Posts
+            </Sidebar.Item>
+          </Link>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
-);
+  );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar.jsx";
 import DashProfile from "../components/DashProfile.jsx";
+import DashPosts from "../components/DashPosts.jsx";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -20,5 +21,7 @@ export default function Dashboard() {
       </div>
       {/* profile .... */}
       {tab === "profile" && <DashProfile />}
+      {/*Posts*/}
+      {tab === "posts" && <DashPosts />}
     </div>;
 }
