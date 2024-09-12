ac74105d5fff7525a4d991dd611eec860f78489f
Aman06-tech <210160203026.aman@gdgu.org>
2024-09-12 18:31:04 +0530

    created post  page ui

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,6 +1,67 @@
+import { Table,  TableRow } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function DashPosts() {
-  return (
-    <div>DashPosts</div>
-  )
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  return <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+    {currentUser.isAdmin && userPosts.length > 0 ?(
+      <>
+      <Table hoverable className="shadow-md">
+        <Table.Head>
+          <Table.HeadCell>Date Updated</Table.HeadCell>
+          <Table.HeadCell>Post Image</Table.HeadCell>
+          <Table.HeadCell>Post Title</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Delete</Table.HeadCell>
+          <Table.HeadCell><span>Edit</span></Table.HeadCell>
+        </Table.Head>
+       {userPosts.map((post) =>(
+        <Table.Body key={post.updatedAt} className="divide-y">
+         <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+          <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
+          <Table.Cell>
+            <Link className="font-medium text-cyan-500"  to={`/post/${post.slug}`}>
+            <img 
+             src={post.image}
+             alt={post.title}
+             className="w-20 h-10 object-cover bg-gray-500"
+            />
+            </Link>
+          </Table.Cell>
+          <Table.Cell><Link className="text-teal-500" to={`/post/${post.slug}`}>{post.title}</Link></Table.Cell>
+          <Table.Cell>{post.category}</Table.Cell>
+          <Table.Cell><span className="text-red-500 hover:underline cursor-pointer">Delete</span></Table.Cell>
+          <Table.Cell><Link className="text-yellow-500 hover:underline cursor-pointer" to={`/update-post/${post._id}`}><span>Edit</span></Link></Table.Cell>
+         </TableRow>
+        </Table.Body>
+      ))}
+
+      
+      </Table>
+      </>
+    ):(
+     <p>No post yet</p>
+    )}
+  </div>;
 }
