268cf15d15c1038bd101d77a4cced72034a9b54d
Aman06-tech <210160203026.aman@gdgu.org>
2024-09-15 15:58:50 +0530

    add show more functionality to post page

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,6 +15,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,22 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () =>{
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if(data.posts.length<9){
+          setShowMore(false);
+        }
+      }
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
     {currentUser.isAdmin && userPosts.length > 0 ?(
       <>
@@ -56,9 +76,12 @@ export default function DashPosts() {
          </TableRow>
         </Table.Body>
       ))}
-
-      
       </Table>
+      {
+        showMore && (
+          <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show more</button>
+        )
+      }
       </>
     ):(
      <p>No post yet</p>
