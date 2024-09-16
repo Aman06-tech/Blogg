d4d38508528ddab30006ec27ee06877ac8fdf971
Aman06-tech <210160203026.aman@gdgu.org>
2024-09-16 23:57:02 +0530

    add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute.jsx";
 import CreatePost from "./pages/CreatePost.jsx";
+import UpdatePost from "./pages/updatePost.jsx";
 
 
 export default function App() {
@@ -27,6 +28,7 @@ export default function App() {
           </Route>
           <Route element={<OnlyAdminPrivateRoute />}>
           <Route path="/create-post"  element={<CreatePost />} />
+          <Route path="/update-post/:postId"  element={<UpdatePost />} />
           </Route>
           <Route path="/projects" element={<Projects />} />
         </Route>
